4065baf446a22b0d1c9475cee6dbfb0c48682d88
Vinicius de Andrade <viniciusdandrade01@gmail.com>
2022-03-31 21:36:15 -0300

    *

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 function wordRank(str) {
     /*  Objetivo da função é 
       1- Receber uma string e transformar em um array de palavras (stringAsArray) 
-      2- A pontuação (de acordo com o desafio) de todas as palavras registradas desse Array são atribuidas a um novo array na mesma ordem utilizando de loops for
-      3- Por fim a função busca pelo maior valor no array das pontuacoes e como as posições de ambos os arrays são as mesmas basta chamar o array da frase com o index do maior valor do array com as pontuações
+      2- A pontuação (de acordo com o desafio) de todas as palavras registradas desse Array são atribuidas a um novo array na mesma ordem
+         utilizando de loops for
+      3- Por fim a função busca pelo maior valor no array das pontuacoes e como as posições de ambos os arrays são as mesmas basta chamar 
+         o array da frase com o index do maior valor do array com as pontuações
     */
 
     let alphabet = 'abcdefghijklmnopqrstuvwxyz'
